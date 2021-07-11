ee817146c923c83d350a135c163f4c4b72cad981
Callum Hill <ccahill.is@gmail.com>
2021-07-11 15:08:29 +1000

    DatePicker Initial

--- DatePickerTutorial/ViewController.swift
+++ DatePickerTutorial/ViewController.swift
@@ -1,17 +1,34 @@
-//
-//  ViewController.swift
-//  DatePickerTutorial
-//
-//  Created by CallumHill on 11/7/21.
-//
-
 import UIKit
 
-class ViewController: UIViewController {
-
-	override func viewDidLoad() {
+class ViewController: UIViewController
+{
+	@IBOutlet weak var dateTF: UITextField!
+	
+	override func viewDidLoad()
+	{
 		super.viewDidLoad()
-		// Do any additional setup after loading the view.
+		
+		let datePicker = UIDatePicker()
+		datePicker.datePickerMode = .date
+		datePicker.addTarget(self, action: #selector(dateChange(datePicker:)), for: UIControl.Event.valueChanged)
+		datePicker.frame.size = CGSize(width: 0, height: 300)
+		datePicker.preferredDatePickerStyle = .wheels
+		datePicker.maximumDate = Date()
+		
+		dateTF.inputView = datePicker
+		dateTF.text = formatDate(date: Date()) // todays Date
+	}
+	
+	@objc func dateChange(datePicker: UIDatePicker)
+	{
+		dateTF.text = formatDate(date: datePicker.date)
+	}
+	
+	func formatDate(date: Date) -> String
+	{
+		let formatter = DateFormatter()
+		formatter.dateFormat = "MMMM dd yyyy"
+		return formatter.string(from: date)
 	}
 
 
